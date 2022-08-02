e91912cf5cfc40fcb65c10d66fb8697580f0793c
Hiroki Murakami <a2241hm@aiit.ac.jp>
2022-08-02 21:50:36 +0900

    lesson15 shell update

--- same-test.sh
+++ same-test.sh
@@ -1,1 +1,31 @@
-#!/bin/bash
+#!/bin/bash  
+
+ans="/tmp/$$-ans"
+result="/tmp/$$-result"
+err="/tmp/$$-error"
+
+ERROR_EXIT(){
+	echo $1
+#	cat ${err}
+#	rm /tmp/$$-*
+# 	exit 1
+}
+
+# テスト1 正常動作の確認
+# 同じ場合
+echo "same" > ${ans}                #出力してほしい内容（手入力）
+./same.sh a a > ${result}           #実際の出力
+diff ${ans} ${result} || ERROR_EXIT "error in 1-1" >> ${err}
+
+# 違う場合
+
+echo "same" > ${ans}
+./same.sh a b > ${result}
+diff ${ans} ${result} || ERROR_EXIT "error in 1-2" >> ${err}
+
+if [ -f ${err} ]; then #エラーログが一つでもあれば
+	cat ${err}         # 中身を表示して
+	rm /tmp/$$-*		# 後片付けをして
+	exit 1				# 終了
+fi
+
--- same.sh
+++ same.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if [ $1 = $2 ];
+then
+	echo "同じです。"
+else
+	echo "同じではありません"
+fi
+
+
+
+
